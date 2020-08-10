410c5c0e9bd3b0e5bac6b1a01f504534de5e3b4f
Zain <zainiscoding@gmail.com>
2020-08-10 13:24:38 +0900

    Cleaned up the Javascript

--- eas-script.js
+++ eas-script.js
@@ -1,16 +1,18 @@
+//Prevents dragging
 window.ondragstart = function() {
     return false;
 }
+
 const resetBtn = document.querySelector("#btn");
 const etchASketch = document.querySelector("#etchASketch");
 const gridContainer = document.querySelector("#grid-container");
 
+//Creates the grid container
 const createGridContainer = () => {
     const gridContainer = document.createElement("div");
     gridContainer.id = "grid-container"
     etchASketch.appendChild(gridContainer);
 }
-createGridContainer();
 
 function setGridSize(gridSize){
     for (let i = 0; i < gridSize * gridSize; i++) {
@@ -23,18 +25,12 @@ function setGridSize(gridSize){
     }  
 }
 
-function clearGrid() {
-    let hoveredGrid = document.querySelectorAll(".hovered");
-    for (let i = 0; i < hoveredGrid.length; i++) {
-        hoveredGrid[i].setAttribute("class", "gridBlock");
-    }
-}
-
+//Reset button functionality
 resetBtn.addEventListener("click", e =>{
     const gridContainer = document.querySelector("#grid-container");
     gridContainer.remove();
     createGridContainer();
-    setGridSize(prompt("Enter grid size number (Eg. entering '16' creates a 16x16 grid)"));
+    setGridSize(prompt("Enter grid size (Eg. '16' creates a 16x16 grid)"));
     const gridBlocks = document.querySelectorAll(".gridBlock");
 
 
@@ -45,8 +41,11 @@ gridBlocks.forEach((gridBlock) => {
 });
 });
 
+//Initial settings
+createGridContainer();
 setGridSize(16);
 
+//Hover to draw function
 const gridBlocks = document.querySelectorAll(".gridBlock");
 gridBlocks.forEach((gridBlock) => {
     gridBlock.addEventListener("mouseenter", function(e) {   
